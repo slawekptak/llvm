f9d3ccae4d9368b34deefd73489acb1982985694
Ptak, Slawomir <slawomir.ptak@intel.com>
2026-05-22 19:25:07 +0000

    Put NumBytes in handle_data, minor changes.

--- sycl/source/ipc_physical_memory.cpp
+++ sycl/source/ipc_physical_memory.cpp
@@ -8,8 +8,11 @@
 
 #include <detail/adapter_impl.hpp>
 #include <detail/context_impl.hpp>
+#include <detail/physical_mem_impl.hpp>
 #include <sycl/context.hpp>
 #include <sycl/ext/oneapi/experimental/ipc_physical_memory.hpp>
+
+#include <cstring>
 
 namespace sycl {
 inline namespace _V1 {
@@ -24,7 +27,49 @@ openIPCPhysicalMemHandle(const std::byte *HandleData, size_t HandleDataSize,
         sycl::make_error_code(errc::feature_not_supported),
         "Device does not support aspect::ext_oneapi_ipc_physical_memory.");
 
-  return sycl::ext::oneapi::experimental::physical_mem{Dev, Ctx, 0};
+  auto CtxImpl = sycl::detail::getSyclObjImpl(Ctx);
+  sycl::detail::adapter_impl &Adapter = CtxImpl->getAdapter();
+
+  // Extract size_t from the beginning of the buffer
+  size_t NumBytes;
+  std::memcpy(&NumBytes, HandleData, sizeof(size_t));
+
+  // Actual UR handle starts after the size_t prefix
+  const std::byte *ActualHandleData = HandleData + sizeof(size_t);
+  size_t ActualHandleSize = HandleDataSize - sizeof(size_t);
+
+  // TODO: UMF and UR currently requires the handle data to be non-const, so we
+  //       need const-cast the data pointer. Once this has been changed, the
+  //       const-cast can be removed.
+  std::byte *NonConstActualHandleData =
+      const_cast<std::byte *>(ActualHandleData);
+
+  ur_physical_mem_handle_t PhysMemHandle = nullptr;
+  ur_result_t UrRes = UR_RESULT_SUCCESS;
+  // TODO IPC physical memory
+  /*
+      Adapter.call_nocheck<sycl::detail::UrApiKind::urIPCOpenPhysMemHandleExp>(
+          CtxImpl->getHandleRef(), getSyclObjImpl(Dev)->getHandleRef(),
+          NonConstActualHandleData, ActualHandleSize, &PhysMemHandle);
+  */
+  if (UrRes == UR_RESULT_ERROR_INVALID_VALUE)
+    throw sycl::exception(
+        sycl::make_error_code(errc::invalid),
+        "HandleData data size does not correspond to the target platform's "
+        "IPC physical memory handle size.");
+  Adapter.checkUrResult(UrRes);
+  if (PhysMemHandle == nullptr)
+    throw sycl::exception(
+        sycl::make_error_code(errc::runtime),
+        "urIPCOpenPhysMemHandleExp returned success but did not produce a "
+        "valid physical memory handle.");
+
+  auto PhysMemImpl = std::make_shared<sycl::detail::physical_mem_impl>(
+      *getSyclObjImpl(Dev), Ctx, NumBytes, PhysMemHandle);
+  return sycl::detail::createSyclObjFromImpl<
+      ext::oneapi::experimental::physical_mem>(PhysMemImpl);
+
+  // return sycl::ext::oneapi::experimental::physical_mem{Dev, Ctx, 0};
 }
 
 } // namespace detail
@@ -32,13 +77,62 @@ openIPCPhysicalMemHandle(const std::byte *HandleData, size_t HandleDataSize,
 namespace ext::oneapi::experimental::ipc::physical_memory {
 
 __SYCL_EXPORT handle get(physical_mem &physmem) {
+  if (!physmem.ipc_enabled())
+    throw sycl::exception(
+        sycl::make_error_code(errc::invalid),
+        "physical_mem was not created with inter-process sharing enabled "
+        "via the enable_ipc property.");
+
+  auto PhysMemImpl = sycl::detail::getSyclObjImpl(physmem);
+  auto CtxImpl = sycl::detail::getSyclObjImpl(physmem.get_context());
+  sycl::detail::adapter_impl &Adapter = CtxImpl->getAdapter();
+
+  // Get the UR handle from the adapter
   void *HandlePtr = nullptr;
   size_t HandleSize = 0;
+  auto UrRes = UR_RESULT_SUCCESS;
+  // TODO IPC physical memory
+  /*
+        Adapter.call_nocheck<sycl::detail::UrApiKind::urIPCGetPhysMemHandleExp>(
+          CtxImpl->getHandleRef(), PhysMemImpl->getHandleRef(), &HandlePtr,
+          &HandleSize);
+      */
+  Adapter.checkUrResult(UrRes);
+  if (HandlePtr == nullptr)
+    throw sycl::exception(
+        sycl::make_error_code(errc::runtime),
+        "urIPCGetPhysMemHandleExp returned success but did not produce a "
+        "valid IPC handle.");
 
-  return {HandlePtr, HandleSize};
+  // Get the size from the physical_mem object
+  size_t NumBytes = physmem.size();
+
+  // Allocate combined buffer: size_t + handle
+  size_t TotalSize = sizeof(size_t) + HandleSize;
+  std::byte *CombinedData = new std::byte[TotalSize];
+
+  // Copy size first, then handle data
+  std::memcpy(CombinedData, &NumBytes, sizeof(size_t));
+  std::memcpy(CombinedData + sizeof(size_t), HandlePtr, HandleSize);
+
+  return {CombinedData, TotalSize};
 }
 
-__SYCL_EXPORT void put(handle &ipc_handle, const sycl::context &ctx) {}
+__SYCL_EXPORT void put(handle &ipc_handle, const sycl::context &ctx) {
+  // Extract actual handle pointer (skip size_t prefix)
+  std::byte *ActualHandleData = ipc_handle.MData + sizeof(size_t);
+
+  auto CtxImpl = sycl::detail::getSyclObjImpl(ctx);
+  sycl::detail::adapter_impl &Adapter = CtxImpl->getAdapter();
+
+  ur_result_t UrRes = UR_RESULT_SUCCESS;
+  // TODO IPC physical memory
+  /*
+  Adapter.call_nocheck<sycl::detail::UrApiKind::urIPCPutPhysMemHandleExp>(
+      CtxImpl->getHandleRef(), ActualHandleData);
+  */
+  Adapter.checkUrResult(UrRes);
+}
 
 } // namespace ext::oneapi::experimental::ipc::physical_memory
 } // namespace _V1
